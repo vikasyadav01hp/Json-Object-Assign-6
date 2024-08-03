0f64747215eab1a7dbcbbe3f3836d07d58b1ee0d
vikasyadav01hp <vikasyadav01.hp@outlook.com>
2024-08-03 12:01:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const person={
+    first_name: "vikas",
+    last_name: "yadav",
+    dob:"10-07-2003"
+}
+
+document.getElementById("first_name").textContent=person.first_name;
+document.getElementById("last_name").innerHTML=person.last_name;
+document.getElementById("dob").innerHTML=person.dob;
